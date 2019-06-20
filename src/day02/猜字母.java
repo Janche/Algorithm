135187e0ce9238037099c838c4cf6799b724a5bc
Janche <957671816@qq.com>
2019-06-20 21:24:51 +0800

    加入 反射对象的获取

--- src/day02/猜字母.java
+++ src/day02/猜字母.java
@@ -7,44 +7,51 @@ import java.util.Scanner;
 该程序类似与约瑟夫环的问题
 */ 
 public class 猜字母 { 
-public static void main(String[] args) { 
-   Scanner s = new Scanner(System.in); 
-   String str = "abcdefghijklmnopqrs"; 
-   String str1 = ""; 
-   for(int i = 0;i < 106;i++){ 
-       str1 = str1 + str; 
-       } 
-   System.out.println(str1.length()); 
-   boolean[] arr = new boolean[str1.length()]; 
-   for(int i=0; i<arr.length; i++) { 
-       arr[i] = true;             //下标为TRUE时说明字母还在圈里
-   } 
-   int leftCount = str1.length(); 
-   int countNum = 0; 
-   int index = 0; 
-   while(leftCount > 1) 
-   { 
-       if(arr[index] == true) {           //当在圈里时
-           if(countNum%2 == 0) {          //下标为偶数时
-               arr[index] = false;        //该字母退出圈子
-               leftCount --;              //剩余字母数目减一
-               } 
-           countNum++; 
-       }
-       index ++;            //每报一次数，下标加一
-       if(index == str1.length()) //是循环数数，当下标大于n，说明已经数了一圈，
-       {    
-          index = 0;                  //将下标设为零重新开始。
-          countNum = 0; 
-       } 
-   } 
-   for(int i=0; i<str1.length(); i++) { 
-       if(arr[i] == true) { 
-    	  System.out.println(i);   //输出结果表示下标为1023(第1024个)的字母,即：q 
-          System.out.println(str1.toCharArray()[i]);
-       } 
-       } 
-   } 
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = "abcdefghijklmnopqrs";
+        String str1 = "";
+        for(int i = 0;i < 106;i++){
+           str1 = str1 + str;
+        }
+        System.out.println(str1.length());
+        boolean[] arr = new boolean[str1.length()];
+        for(int i=0; i<arr.length; i++) {
+           //下标为TRUE时说明字母还在圈里
+           arr[i] = true;
+        }
+        int leftCount = str1.length();
+        int countNum = 0;
+        int index = 0;
+        while(leftCount > 1) {
+           //当在圈里时
+           if(arr[index] == true) {
+               //下标为偶数时
+               if(countNum%2 == 0) {
+                   //该字母退出圈子
+                   arr[index] = false;
+                   //剩余字母数目减一
+                   leftCount --;
+               }
+               countNum++;
+           }
+           //每报一次数，下标加一
+           index ++;
+           //是循环数数，当下标大于n，说明已经数了一圈，
+           if(index == str1.length()) {
+               //将下标设为零重新开始。
+              index = 0;
+              countNum = 0;
+           }
+        }
+        for(int i=0; i<str1.length(); i++) {
+           if(arr[i] == true) {
+               //输出结果表示下标为1023(第1024个)的字母,即：q
+              System.out.println(i);
+              System.out.println(str1.toCharArray()[i]);
+           }
+        }
+    }
 /*
 	@Test
 	public void test(){
