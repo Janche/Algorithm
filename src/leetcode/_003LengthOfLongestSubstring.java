03d3138b965944215be21cbc244c9502c051c0ab
Janche <957671816@qq.com>
2019-12-15 00:12:04 +0800

    :sparkles: 更新

--- src/leetcode/_003LengthOfLongestSubstring.java
+++ src/leetcode/_003LengthOfLongestSubstring.java
@@ -12,7 +12,77 @@ import java.util.Map;
 public class _003LengthOfLongestSubstring {
 
     public static void main(String[] args) {
-        String s = "pwwkew";
+
+        test01();
+
+        test02();
+
+        test03();
+
+        test04();
+
+    }
+
+    private static void test04() {
+        String s = "sdabcdgjabklabcjg3abb24";
+        int ans = 0;
+        Map<Character, Integer> map = new HashMap<>(16);
+        for (int end = 0, start = 0; end < s.length(); end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        System.out.println(ans);
+    }
+
+    private static void test03() {
+        String s = "abcabcdabfhua";
+        Map<Character,Integer> map = new HashMap<>(16);
+        int ans = 0;
+        for (int start =0, i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))){
+                int temp = map.get(s.charAt(i)) + 1;
+                for (int j = start; j < temp; j++) {
+                    map.remove(s.charAt(j));
+                }
+                start = temp;
+            }
+            map.put(s.charAt(i), i);
+            ans = Math.max(ans, i - start + 1);
+        }
+        System.out.println(ans);
+
+    }
+
+    private static void test02() {
+        String s = "abcabcdabfhua";
+        Map<Character,Integer> map = new HashMap<>(16);
+        int start = 0, end = 0, max = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (map.containsKey(chars[i])){
+                int temp = map.get(chars[i]) + 1;
+                for (int j = start; j < temp; j++) {
+                    map.remove(chars[j]);
+                }
+                start = temp;
+                end = i;
+            }
+            map.put(chars[i], i);
+            end++;
+            if (max < end -start){
+                max = end - start;
+            }
+        }
+        System.out.println(max);
+
+    }
+
+    private static void test01() {
+        String s = "abcabcbb";
         int max = 0;
         String str = "";
         for (int i = 0; i < s.length(); i++) {
