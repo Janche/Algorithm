2cc6ac15df3b7908545136c435848926e9bf035d
Janche <957671816@qq.com>
2020-04-22 15:06:40 +0800

    :sparkles: 更新二叉树

--- src/leetcode/_099RecoverTree.java
+++ src/leetcode/_099RecoverTree.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author lirong
  * @ClassName: _099RecoverTree
@@ -28,48 +32,77 @@ public class _099RecoverTree {
         node.left = new TreeNode(1);
         node.right = new TreeNode(4);
         node.right.left = new TreeNode(2);
-        reverse(node);
-        recoverTree(node, 0, 0);
-        System.out.println(a+", "+b);
-        // reverse(recoverNode);
+        List<Integer> list = new ArrayList<>();
+        inorder(node,list);
+        int[] twoSwapped = findTwoSwapped(list);
+        System.out.println();
+        Arrays.stream(twoSwapped).forEach(System.out::print);
+        reverse(node, twoSwapped);
+        System.out.println();
+        inorder(node,list);
     }
 
-    static int[] arr = new int[3];
-    static int a = 0;
-    static int b = 0;
-
-    private static int recoverTree(TreeNode node, int leftMax, int rightMin) {
-        if (null == node){
-            return 0;
-        }
-        int leftVal = recoverTree(node.left, leftMax, rightMin);
+    private static void reverse(TreeNode node, int[] arr) {
+        if (null == node) return;
         int val = node.val;
-        leftMax = val;
-        if (leftVal != 0 && val < leftVal){
-            a = val;
+        if (val == arr[0]){
+            node.val = arr[1];
+        }else if(val == arr[1]){
+            node.val = arr[0];
         }
-        rightMin = leftVal;
-        int rightVal = recoverTree(node.right, leftMax, rightMin);
-
-        if (rightVal != 0 && val > rightVal){
-            b = val;
-        }
-
-        if (val > rightMin || val < leftMax){
-            System.out.println();
-            System.out.print(leftVal);
-            System.out.print(val);
-            System.out.print(rightVal);
-            System.out.println();
-        }
-        return node.val;
+        reverse(node.left, arr);
+        reverse(node.right, arr);
     }
 
-    private static void reverse(TreeNode node) {
-        if (null != node){
-            System.out.println(node.val);
-            reverse(node.left);
-            reverse(node.right);
+    public static void inorder(TreeNode root, List<Integer> nums) {
+        if (root == null) return;
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    }
+
+    public static int[] findTwoSwapped(List<Integer> nums) {
+        int n = nums.size();
+        int x = -1, y = -1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums.get(i) > nums.get(i + 1)){
+                y = nums.get(i + 1);
+                if (x == -1) {
+                    x = nums.get(i);
+                }else {
+                    break;
+                }
+            }
         }
+        return new int[]{x, y};
+    }
+
+    static TreeNode x, y, pred;
+
+    /**
+     * 直接找出两个需要交换的节点
+     * @param root
+     */
+    public static void findTwoSwapped(TreeNode root) {
+        if (null == root) return;
+        findTwoSwapped(root.left);
+        if (pred != null && pred.val > root.val){
+            y = root;
+            if (x == null) x = pred;
+            else return;
+        }
+        pred = root;
+        findTwoSwapped(root.right);
+    }
+
+    public static void swap(TreeNode a, TreeNode b){
+        int temp = a.val;
+        a.val = b.val;
+        b.val = temp;
+    }
+
+    public static void reverse(TreeNode root) {
+       findTwoSwapped(root);
+       swap(x, y);
     }
 }
